e57cbfb2f737cf576e04a220a8b2819f1b879f67
rikvanvelzen <rikvanvelzen@yahoo.com>
2020-09-05 16:10:18 +0200

    heroku server added!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="app">
       <div className="app__videos">
         <Video
-          url="https://v16m.tiktokcdn.com/7e6c0e953aa243de7b386adf40e6cd83/5f52bdf0/video/tos/useast2a/tos-useast2a-ve-0068c001/259b1391c9b94d65b5ae37beb7468191/?a=1233&br=3492&bt=1746&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009041621220101890740125A004A87&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2c8dXdtM2lndzMzZTczM0ApZWQ5PDRlaWQ1NzM1Nmc0N2dicF5rZS1vNjVfLS1eMTZzcy8wLzIzNWEtMV8uMzIzLS86Yw%3D%3D&vl=&vr="
+          url="https://v16m.tiktokcdn.com/4f62584c86c016645da824402e84751c/5f53c0cc/video/tos/useast2a/tos-useast2a-pve-0068/ad030ff6abdf41549b065f9ff7368bea/?a=1233&br=5484&bt=2742&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090510451701019020922006143CF4&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajZ3dng2OW54djMzZjczM0ApPGc1aTc8aDxkNzw1OWZkZWdtbGFjYnM0cGtfLS0vMTZzcy1gMGMuYC9fLi0wX2IvXzY6Yw%3D%3D&vl=&vr="
           channel="Norakomi!"
           description="This is an epic MERN stack TikTok clone!!"
           song="99 problems but react aint one!"
@@ -17,7 +17,7 @@ function App() {
           shares={352}
         />
         <Video
-          url="https://v16m.tiktokcdn.com/7e6c0e953aa243de7b386adf40e6cd83/5f52bdf0/video/tos/useast2a/tos-useast2a-ve-0068c001/259b1391c9b94d65b5ae37beb7468191/?a=1233&br=3492&bt=1746&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009041621220101890740125A004A87&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2c8dXdtM2lndzMzZTczM0ApZWQ5PDRlaWQ1NzM1Nmc0N2dicF5rZS1vNjVfLS1eMTZzcy8wLzIzNWEtMV8uMzIzLS86Yw%3D%3D&vl=&vr="
+          url="https://v16m.tiktokcdn.com/4f62584c86c016645da824402e84751c/5f53c0cc/video/tos/useast2a/tos-useast2a-pve-0068/ad030ff6abdf41549b065f9ff7368bea/?a=1233&br=5484&bt=2742&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090510451701019020922006143CF4&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajZ3dng2OW54djMzZjczM0ApPGc1aTc8aDxkNzw1OWZkZWdtbGFjYnM0cGtfLS0vMTZzcy1gMGMuYC9fLi0wX2IvXzY6Yw%3D%3D&vl=&vr="
           channel="Norakomi!"
           description="This is an epic MERN stack TikTok clone!!"
           song="99 problems but react aint one!"
